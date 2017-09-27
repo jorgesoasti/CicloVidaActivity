c4926e704f70eb0f829b55358bb263591126bd7f
jorge.soasti <jorgesoasti@gmail.com>
2017-09-27 13:40:05 -0500

    Probando estados con Intents

--- app/src/main/java/com/jorgesoasti/ciclovidaactivity/MainActivity.java
+++ app/src/main/java/com/jorgesoasti/ciclovidaactivity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.jorgesoasti.ciclovidaactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, getResources().getString(R.string.onCreate), Toast.LENGTH_SHORT).show();
+
+        Button btnUno = (Button) findViewById(R.id.btnUno);
+
+        btnUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tvUno = (TextView) findViewById(R.id.tvUno);
+                String texto1 = tvUno.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SegundaActividad.class);
+                intent.putExtra("One", texto1);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -31,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(this, getResources().getString(R.string.onRestart), Toast.LENGTH_SHORT).show();
+        //No funciona en este estado traer datos a la actividad principal
+        //Bundle parametros = getIntent().getExtras();
+        //String texto1 = parametros.getString("Two");
+        //TextView tv1 = (TextView) findViewById(R.id.tvUno);
+        //tv1.setText(texto1);
     }
 
     @Override
---
+++ app/src/main/java/com/jorgesoasti/ciclovidaactivity/SegundaActividad.java
@@ -0,0 +1,76 @@
+package com.jorgesoasti.ciclovidaactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SegundaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_actividad);
+        Toast.makeText(this, getResources().getString(R.string.onCreate) + "2", Toast.LENGTH_SHORT).show();
+
+        final TextView tvDos = (TextView) findViewById(R.id.tvDos);
+
+        Bundle parametros = getIntent().getExtras();
+        String texto1 = parametros.getString("One");
+        tvDos.setText("Pantalla 2 + " + texto1);
+
+        Button btnDos = (Button) findViewById(R.id.btnDos);
+
+        btnDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String texto2 = tvDos.getText().toString();
+                Intent intent = new Intent(SegundaActividad.this, MainActivity.class);
+                intent.putExtra("Two", texto2);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, getResources().getString(R.string.onStart) + "2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, getResources().getString(R.string.onResume) + "2", Toast.LENGTH_SHORT).show();
+    }
+
+    //Actividad Corriendo
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, getResources().getString(R.string.onRestart) + "2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, getResources().getString(R.string.onPause) + "2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, getResources().getString(R.string.onStop) + "2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, getResources().getString(R.string.onDestroy) + "2", Toast.LENGTH_SHORT).show();
+    }
+}
